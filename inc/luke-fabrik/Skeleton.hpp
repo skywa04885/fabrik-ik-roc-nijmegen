bdbf8f2d0515704184f594c9f0a7f7c723b32bba
Luke Rieff <lrieff@fannst.nl>
2021-09-15 15:41:45 +0200

    readme

--- inc/luke-fabrik/Skeleton.hpp
+++ inc/luke-fabrik/Skeleton.hpp
@@ -1,3 +1,14 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
 #include "luke-fabrik/Joint.hpp"
+
+class Skeleton
+{
+protected:
+    std::size_t m_NextID;
+    std::vector <std::shared_ptr <Joint>> m_Heads;
+    std::shared_ptr <Joint> m_Root;
+};
